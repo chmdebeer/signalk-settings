692c81a7c701c7cea39cd83c9d385a86fa5209f0
chmdebeer <chmdebeer@gmail.com>
2022-06-26 22:41:56 -0700

    Shotdown in place

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,13 +4,16 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 
+print("Set BPIO3 to hi and wait for button press")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while (True):
-  print("Set BPIO3 to hi and wait for button press")
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
   GPIO.wait_for_edge(3, GPIO.FALLING)
-  print("shutdown")
-  subprocess.call(['shutdown', '-h', 'now'], shell=False)
-  time.sleep(4)
+  time.sleep(1)
+  if (GPIO.input(3) == 0):
+    print("shutdown")
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+    time.sleep(4)
 
 
