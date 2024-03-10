314b9cbce4e0bd3ddd9c0d7da5d29e9d89c01e9f
Marius <chmdebeer@gmail.com>
2024-03-09 18:49:22 -0800

    pi to boat

--- README.md
+++ README.md
@@ -38,6 +38,7 @@
 
     DEBUG=signalk-server:put,signalk-reflections,signalk-gpio bin/signalk-server
 
-    SIGNALK_NODE_CONFIG_DIR=/home/pi/signalk-settings/.signalk DEBUG=signalk-gsm bin/signalk-server
+    SIGNALK_NODE_CONFIG_DIR=/home/boat/signalk/signalk-settings/.signalk DEBUG=signalk-gsm bin/signalk-server
+    SIGNALK_NODE_CONFIG_DIR=/home/boat/signalk/signalk-settings/.signalk bin/signalk-server
 
     node bin/signalk-server -s settings/reflections.json
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
     // First application
     {
       name : 'reflections',
-      script : '/home/boat/signalk-server/bin/signalk-server',
-      cwd : '/home/boat/signalk-server',
+      script : '/home/boat/signalk/signalk-server/bin/signalk-server',
+      cwd : '/home/boat/signalk/signalk-server',
       env: {
           "PORT": 3000,
-          "SIGNALK_NODE_CONFIG_DIR": "/home/boat/signalk-settings/.signalk"
+          "SIGNALK_NODE_CONFIG_DIR": "/home/boat/signalk/signalk-settings/.signalk"
       }
     }
   ]
